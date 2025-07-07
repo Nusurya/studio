9c16871e947de69c52493bbb4c2a8f6b83e484cf
Surya Prakash Allampalli <surya@nushift.in>
2025-07-07 17:03:55 +0000

    I need to make the 'Approve' and 'Reject' buttons in the details card re

--- src/components/submission-details.tsx
+++ src/components/submission-details.tsx
@@ -188,7 +188,7 @@ export default function SubmissionDetails({
         )}
         {(submission.status === "rejected" ||
           submission.status === "approved") && (
-          <CardFooter className="pt-6">
+          <CardFooter className="grid pt-6">
             <Button
               onClick={handleReReview}
               variant="secondary"
